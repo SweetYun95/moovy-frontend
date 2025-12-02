32e6f50e8b240364562ff1a7ba81ff26a52a5c15
eeinoeyes <loveourjune@gmail.com>
2025-12-02 17:18:03 +0900

    [feat] admin페이지 레이아웃 구현, import 순서 변경 등 대규모 업데이트

---
+++ src/components/admin/AdminTable/ReportTable.tsx
@@ -0,0 +1,73 @@
+// moovy-frontent/src/components/admin/AdminTable/ReportTable.tsx
+
+import React from "react";
+
+import Avatar from "../../../assets/Avatar.png";
+
+interface TableProps {
+  content: string;
+  columns: string[];
+}
+
+const ReportTable: React.FC<TableProps> = ({ content, columns }) => {
+  const tableData = [
+    {
+      report_id: 1,
+      reporter_id: "Natali Craig",
+      reported_id: "Kate Morrison",
+      category: "신고내역",
+      report_content: "신고 내용",
+      created_at: "2025-10-08",
+      state: "처리완료",
+    },
+    {
+      report_id: 1,
+      reporter_id: "Natali Craig",
+      reported_id: "Kate Morrison",
+      category: "신고내역",
+      report_content: "신고 내용",
+      created_at: "2025-10-08",
+      state: "처리완료",
+    },
+    {
+      report_id: 1,
+      reporter_id: "Natali Craig",
+      reported_id: "Kate Morrison",
+      category: "신고내역",
+      report_content: "신고 내용",
+      created_at: "2025-10-08",
+      state: "처리완료",
+    },
+    {
+      report_id: 1,
+      reporter_id: "Natali Craig",
+      reported_id: "Kate Morrison",
+      category: "신고내역",
+      report_content: "신고 내용",
+      created_at: "2025-10-08",
+      state: "처리완료",
+    },
+  ];
+
+  return (
+    <>
+      {/* index 추후 수정 */}
+      {tableData.map((data, index) => (
+        <ul className="data" key={index}>
+          <li>
+            <img src={Avatar} /> {data.reported_id}
+          </li>
+          <li>
+            <img src={Avatar} /> {data.reporter_id}
+          </li>
+          <li>{data.category}</li>
+          <li>{data.report_content}</li>
+          <li>{data.created_at}</li>
+          <li>{data.state}</li>
+        </ul>
+      ))}
+    </>
+  );
+};
+
+export default ReportTable;
