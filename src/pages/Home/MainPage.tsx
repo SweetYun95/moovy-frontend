8731a8a4cd96d72708edb0e8312d506e581cc7e6
SweetYun95 <97324488+SweetYun95@users.noreply.github.com>
2026-03-17 00:39:36 +0900

    [refactor] 댓글 상세 모달 데이터 타입 정리
    - CommentDetailData.id를 필수 필드로 변경
    - buildCommentDetailData에서 id 존재 검증 추가
    - CommentDetailModal과 타입 불일치 문제 해결

--- src/pages/Home/MainPage.tsx
+++ src/pages/Home/MainPage.tsx
@@ -1,10 +1,12 @@
+// moovy-frontend/src/pages/Home/MainPage.tsx
+
 // 외부 라이브러리
 import React, { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
 
 // 내부 유틸/전역/서비스
 import { useDisclosure } from '@/hooks/useDisclosure'
 import { buildCommentDetailData } from '@/utils/modals'
-import type { SliderSection } from '@/components/Home/MovieCard/MovieCardSlider'
 
 // 컴포넌트
 import HeroBanner from '@/components/Home/HeroBanner/HeroBanner'
@@ -12,14 +14,25 @@ import { ImageCommentCardSlider } from '@/components/Home/ImageCommentCard/Image
 import { MovieCardSlider } from '@/components/Home/MovieCard/MovieCardSlider'
 import { QuickMenu } from '@/components/Home/QuickMenu/QuickMenu'
 import { CommentDetailModal } from '@/components/modals/CommentDetailModal/CommentDetailModal'
-import { useDispatch } from 'react-redux'
+import type { SliderSection } from '@/components/Home/MovieCard/MovieCardSlider'
 
 export default function MainPage() {
    const { isOpen: showCommentDetail, open: openCommentDetail, close: closeCommentDetail } = useDisclosure(false)
-   const [commentDetailData, setCommentDetailData] = useState<{ username: string; date: string; content: string; likes: number; replies: number } | null>(null)
+
+   const [commentDetailData, setCommentDetailData] = useState<{
+      id: number
+      username: string
+      date: string
+      content: string
+      likes: number
+      replies: number
+      profileImageUrl?: string
+   } | null>(null)
+
    const [movieSections, setMovieSections] = useState<SliderSection[]>([])
 
    const dispatch = useDispatch()
+
    useEffect(() => {}, [dispatch])
 
    return (
@@ -52,6 +65,7 @@ export default function MainPage() {
                />
             </div>
          </div>
+
          {commentDetailData && <CommentDetailModal isOpen={showCommentDetail} onClose={closeCommentDetail} commentData={commentDetailData} />}
       </>
    )
